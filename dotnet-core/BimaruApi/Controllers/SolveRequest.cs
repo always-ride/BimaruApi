401492cd9e5817f2b57d8aa95f73e30b99c4866d
Daniel Fahrni <daniel.fahrni@junamin.ch>
2025-09-13 15:16:05 +0200

    small fix

--- dotnet-core/BimaruApi/Controllers/SolveRequest.cs
+++ dotnet-core/BimaruApi/Controllers/SolveRequest.cs
@@ -2,7 +2,7 @@ namespace BimaruApi.Controllers
 {
     public class SolveRequest
     {
-        public string[] BoardLines { get; set; } = Array.Empty<string>();  
+        public string[] BoardLines { get; set; } = [];  
 
         public string Board => string.Join("\n", BoardLines);
     }
